83e2af88b5a933e87e963993e01e8fa04ae8aa43
laborg <laborg@893db81f-18e8-4009-acbb-c64a79be3e64>
2010-10-01 16:54:31 +0000

    version 0.0.10

---
+++ src/java/at/laborg/briss/MergedPanel.java
@@ -0,0 +1,387 @@
+// $Id$
+/**
+ * Copyright 2010 Gerhard Aigner
+ * 
+ * This file is part of BRISS.
+ * 
+ * BRISS is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * BRISS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * BRISS. If not, see http://www.gnu.org/licenses/.
+ */
+package at.laborg.briss;
+
+import java.awt.AlphaComposite;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Composite;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class MergedPanel extends JPanel implements MouseMotionListener,
+		MouseListener {
+
+	// last drawn rectangle. a "ghosting" rectangle will
+	// help the user to create the two equally sized crop rectangles
+	private static CropRect lastCrop;
+	private static CropRect curCrop;
+	private static Point lastDragPoint;
+	private static Point cropStartPoint;
+	private static int dragCropIndex = -1;
+
+	private final static Composite compositeSmooth = AlphaComposite
+			.getInstance(AlphaComposite.SRC_OVER, .2f);
+	private final static Composite compositeXor = AlphaComposite.getInstance(
+			AlphaComposite.SRC_OVER, .8f);
+
+	private final List<CropRect> crops = new ArrayList<CropRect>();
+	private final PDFPageCluster cluster;
+	private final BufferedImage img;
+
+	public MergedPanel(PDFPageCluster cluster) {
+		super();
+		this.cluster = cluster;
+		this.img = cluster.getPreviewImage();
+		setPreferredSize(new Dimension(img.getWidth(), img.getHeight()));
+		addMouseMotionListener(this);
+		addMouseListener(this);
+
+		setToolTipText(createInfoString(cluster));
+	}
+
+	private String createInfoString(PDFPageCluster cluster) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("<html>");
+		sb.append(cluster.isEvenPage() ? "Even " : "Odd ").append("page<br>");
+		sb.append(cluster.getAllPages().size() + " pages: ");
+		int pagecounter = 0;
+		for (Integer pageNumber : cluster.getAllPages()) {
+			sb.append(pageNumber + " ");
+			if (pagecounter++ > 10) {
+				pagecounter = 0;
+				sb.append("<br>");
+			}
+		}
+		sb.append("</html>");
+		return sb.toString();
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		update(g);
+	}
+
+	@Override
+	public void update(Graphics g) {
+		if (!isEnabled())
+			return;
+
+		Graphics2D g2 = (Graphics2D) g;
+		g2.drawImage(img, null, 0, 0);
+
+		// draw previously created rectangles
+		int cropCnt = 0;
+		Font currentFont = g2.getFont();
+		g2.setComposite(compositeSmooth);
+		for (CropRect crop : crops) {
+			g2.setColor(Color.BLUE);
+			g2.fill(crop);
+
+			g2.setColor(Color.BLACK);
+			Font scaledFont = scaleFont(String.valueOf(cropCnt++), crop, g2,
+					currentFont);
+			g2.setFont(scaledFont);
+			g2
+					.drawString(String.valueOf(cropCnt), crop.x, crop.y
+							+ crop.height);
+
+			if (crop.isSelected()) {
+				g2.setComposite(compositeXor);
+				g2.setColor(Color.BLACK);
+
+				scaledFont = scaleFont("Selected", crop, g2, currentFont);
+				g2.setFont(scaledFont);
+				g2.fill(crop);
+				g2.setColor(Color.YELLOW);
+				g2.setComposite(compositeSmooth);
+				g2.drawString("Selected", crop.x, crop.y + crop.height);
+			}
+		}
+
+		g2.setColor(Color.BLUE);
+		if (curCrop != null) {
+			g2.fill(curCrop);
+			if (lastCrop != null) {
+				// draw "ghost"-rectangle to show the dimension of the last crop
+				g2.setColor(Color.GREEN);
+				g2.setStroke(new BasicStroke(3.0f));
+				g2.setComposite(compositeXor);
+				g2.drawRect(curCrop.x, curCrop.y, lastCrop.width,
+						lastCrop.height);
+			}
+		}
+
+		g2.dispose();
+
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent mE) {
+		Point curPoint = mE.getPoint();
+
+		if (dragCropIndex == -1) {
+			if (cropStartPoint == null) {
+				cropStartPoint = curPoint;
+			}
+			// create the rectangle to draw
+			curCrop = new CropRect();
+			curCrop.x = (curPoint.x < cropStartPoint.x) ? curPoint.x
+					: cropStartPoint.x;
+			curCrop.width = Math.abs(curPoint.x - cropStartPoint.x);
+			curCrop.y = (curPoint.y < cropStartPoint.y) ? curPoint.y
+					: cropStartPoint.y;
+			curCrop.height = Math.abs(curPoint.y - cropStartPoint.y);
+			// // cut it to the image border
+			// curCrop = curCrop.intersection(new Rectangle(0, 0,
+			// img.getWidth(),
+			// img.getHeight()));
+		} else {
+			if (lastDragPoint == null)
+				lastDragPoint = curPoint;
+			// drag the rectangle around
+			crops.get(dragCropIndex).translate(curPoint.x - lastDragPoint.x,
+					curPoint.y - lastDragPoint.y);
+			lastDragPoint = curPoint;
+			// crops.set(dragCropIndex, crops.get(dragCropIndex).intersection(
+			// new Rectangle(0, 0, img.getWidth(), img.getHeight())));
+		}
+
+		repaint();
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent arg0) {
+
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent mE) {
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+	}
+
+	@Override
+	public void mousePressed(MouseEvent mE) {
+		Point p = mE.getPoint();
+
+		if (mE.isControlDown()) {
+			for (CropRect crop : crops) {
+				if (crop.contains(p)) {
+					crop.setSelected(!crop.isSelected());
+				}
+			}
+			repaint();
+			return;
+		}
+		if (mE.getButton() == MouseEvent.BUTTON1) {
+			// check if the click was made in a crop rectangle
+			for (Rectangle crop : crops) {
+				if (crop.contains(p)) {
+					dragCropIndex = crops.indexOf(crop);
+				}
+			}
+			if (dragCropIndex == -1) {
+				cropStartPoint = p;
+			}
+		} else {
+			int deleteIndex = -1;
+			for (Rectangle crop : crops) {
+				if (crop.contains(p)) {
+					deleteIndex = crops.indexOf(crop);
+				}
+			}
+			if (deleteIndex != -1) {
+				crops.remove(deleteIndex);
+			}
+			cluster.clearRatios();
+			repaint();
+		}
+	}
+
+	public int getWidestRect() {
+		int max = -1;
+		for (CropRect crop : crops) {
+			if (crop.isSelected()) {
+				if (crop.width > max) {
+					max = crop.width;
+				}
+			}
+		}
+		return max;
+	}
+
+	public int getHeighestRect() {
+		int max = -1;
+		for (CropRect crop : crops) {
+			if (crop.isSelected()) {
+				if (crop.height > max) {
+					max = crop.height;
+				}
+			}
+		}
+		return max;
+	}
+
+	public void setSelCropWidth(int width) {
+		for (CropRect crop : crops) {
+			if (crop.isSelected()) {
+				int diffToMax = width - crop.width;
+				crop.grow(diffToMax / 2, 0);
+			}
+		}
+		updateClusterRatios(crops);
+		repaint();
+	}
+
+	public void setSelCropHeight(int height) {
+		for (CropRect crop : crops) {
+			if (crop.isSelected()) {
+				int diffToMax = height - crop.height;
+				crop.grow(0, diffToMax / 2);
+			}
+		}
+		updateClusterRatios(crops);
+		repaint();
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// add croprectangle to list
+		if (curCrop != null) {
+			crops.add(curCrop);
+			lastCrop = curCrop;
+		}
+		// throw away all crops which are to small
+		List<Rectangle> cropsToTrash = new ArrayList<Rectangle>();
+		for (Rectangle crop : crops) {
+			if (crop.getWidth() < 5 || crop.getHeight() < 5) {
+				cropsToTrash.add(crop);
+			}
+		}
+		crops.removeAll(cropsToTrash);
+		updateClusterRatios(crops);
+		cropStartPoint = null;
+		lastDragPoint = null;
+		dragCropIndex = -1;
+		curCrop = null;
+		repaint();
+	}
+
+	private void updateClusterRatios(List<CropRect> tmpCrops) {
+		cluster.clearRatios();
+		for (Rectangle crop : tmpCrops) {
+			cluster.addRatios(getCutRatiosForPdf(crop, img.getWidth(), img
+					.getHeight()));
+		}
+	}
+
+	/**
+	 * creates the crop ratios from the user selection. 0 = left 1 = bottom 2 =
+	 * right 3 = top
+	 * 
+	 * @param crop
+	 * 
+	 * @return the cropped ratios or null if to small
+	 */
+	private static Float[] getCutRatiosForPdf(Rectangle crop, int imgWidth,
+			int imgHeight) {
+		int x1, x2, y1, y2;
+
+		x1 = crop.x;
+		x2 = x1 + crop.width;
+		y1 = crop.y;
+		y2 = y1 + crop.height;
+
+		// check for maximum and minimum
+		if (x1 < 0) {
+			x1 = 0;
+		}
+		if (x2 > imgWidth) {
+			x2 = imgWidth;
+		}
+		if (y1 < 0) {
+			y1 = 0;
+		}
+		if (y2 > imgHeight) {
+			y2 = imgHeight;
+		}
+
+		Float[] ratios = new Float[4];
+		// left
+		ratios[0] = (float) x1 / imgWidth;
+		// bottom
+		ratios[1] = (float) (imgHeight - y2) / imgHeight;
+		// right
+		ratios[2] = 1 - ((float) x2 / imgWidth);
+		// top
+		ratios[3] = 1 - ((float) (imgHeight - y1) / imgHeight);
+
+		return ratios;
+	}
+
+	private static Font scaleFont(String text, Rectangle rect, Graphics g,
+			Font pFont) {
+
+		Font font = pFont;
+		float fontSize = (float) font.getSize();
+		font = g.getFont().deriveFont(fontSize);
+		int width = g.getFontMetrics(font).stringWidth(text);
+		int height = g.getFontMetrics(font).getHeight();
+		float scaleFactorWidth = rect.width / width;
+		float scaleFactorHeight = rect.height / height;
+		float scaledWidth = (scaleFactorWidth * fontSize);
+		float scaledHeight = (scaleFactorHeight * fontSize);
+		return g.getFont().deriveFont(
+				(scaleFactorHeight > scaleFactorWidth) ? scaledWidth
+						: scaledHeight);
+	}
+
+	private class CropRect extends Rectangle {
+		private boolean selected = false;
+
+		public boolean isSelected() {
+			return selected;
+		}
+
+		public void setSelected(boolean selected) {
+			this.selected = selected;
+		}
+
+	}
+}
---
+++ src/java/at/laborg/briss/PDFPageCluster.java
@@ -0,0 +1,189 @@
+// $Id$
+/**
+ * Copyright 2010 Gerhard Aigner
+ * 
+ * This file is part of BRISS.
+ * 
+ * BRISS is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * BRISS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * BRISS. If not, see http://www.gnu.org/licenses/.
+ */
+package at.laborg.briss;
+
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PDFPageCluster {
+
+	private final static int MERGE_VARIABILITY = 30;
+	private final static int MAX_MERGE_PAGES = 20;
+	private final static int MAX_PAGE_HEIGHT = 600;
+	private List<Integer> pagesToMerge;
+	private List<Integer> allPages;
+	private BufferedImage previewImage;
+	private WritableRaster raster = null;
+	private double[][] imageData = null;
+	private List<Float[]> cropRatiosList = new ArrayList<Float[]>();
+
+	private boolean evenPage;
+	private int pageWidth;
+	private int pageHeight;
+
+	public PDFPageCluster(boolean isEvenPage, int pageWidth, int pageHeight) {
+		super();
+		this.pageWidth = pageWidth;
+		this.pageHeight = pageHeight;
+		this.evenPage = isEvenPage;
+		this.pagesToMerge = new ArrayList<Integer>();
+	}
+
+	public void addImageToPreview(BufferedImage imageToAdd) {
+		if (previewImage == null) {
+			int pageHeight = imageToAdd.getHeight() > MAX_PAGE_HEIGHT ? MAX_PAGE_HEIGHT
+					: imageToAdd.getHeight();
+			float scaleFactor = (float) pageHeight / imageToAdd.getHeight();
+			int pageWidth = (int) (imageToAdd.getWidth() * scaleFactor);
+			// create the first preview image
+			previewImage = new BufferedImage(pageWidth, pageHeight,
+					BufferedImage.TYPE_BYTE_GRAY);
+			previewImage.getGraphics().drawImage(
+					scaleImage(imageToAdd, pageWidth, pageHeight), 0, 0, null);
+			raster = previewImage.getRaster().createCompatibleWritableRaster();
+			imageData = new double[previewImage.getWidth()][previewImage
+					.getHeight()];
+		}
+		// scale image to the first added
+		average(scaleImage(imageToAdd, previewImage.getWidth(), previewImage
+				.getHeight()), imageData);
+	}
+
+	private static void average(BufferedImage image, double[][] values) {
+		for (int k = 0; k < image.getHeight(); ++k) {
+			for (int j = 0; j < image.getWidth(); ++j) {
+				values[j][k] += image.getRaster().getSample(j, k, 0);
+			}
+		}
+	}
+
+	private static BufferedImage scaleImage(BufferedImage bsrc, int width,
+			int height) {
+		BufferedImage bdest = new BufferedImage(width, height,
+				BufferedImage.TYPE_BYTE_GRAY);
+		Graphics2D g = bdest.createGraphics();
+		AffineTransform at = AffineTransform.getScaleInstance((double) bdest
+				.getWidth()
+				/ bsrc.getWidth(), (double) bdest.getHeight()
+				/ bsrc.getHeight());
+		g.drawRenderedImage(bsrc, at);
+		g.dispose();
+		return bdest;
+	}
+
+	public BufferedImage getPreviewImage() {
+		for (int k = 0; k < previewImage.getHeight(); ++k) {
+			for (int j = 0; j < previewImage.getWidth(); ++j) {
+				raster.setSample(j, k, 0, Math.round(imageData[j][k]
+						/ (getPagesToMerge().size())));
+			}
+		}
+		previewImage.setData(raster);
+		return previewImage;
+	}
+
+	/**
+	 * returns the ratio to crop the page x1,y1,x2,y2, origin = bottom left
+	 * 
+	 * @return
+	 */
+	public List<Float[]> getRatiosList() {
+		return cropRatiosList;
+	}
+
+	public void clearRatios() {
+		cropRatiosList.clear();
+	}
+
+	public void addRatios(Float[] ratios) {
+		cropRatiosList.add(ratios);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (evenPage ? 1231 : 1237);
+		result = prime * result + getRoundedPageHeight();
+		result = prime * result + getRoundedPageWidth();
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PDFPageCluster other = (PDFPageCluster) obj;
+		if (evenPage != other.evenPage)
+			return false;
+		if (getRoundedPageHeight() != other.getRoundedPageHeight())
+			return false;
+		if (getRoundedPageWidth() != other.getRoundedPageWidth())
+			return false;
+		return true;
+	}
+
+	public boolean isEvenPage() {
+		return evenPage;
+	}
+
+	public int getRoundedPageHeight() {
+		int tmp = pageHeight / MERGE_VARIABILITY;
+		return tmp * MERGE_VARIABILITY;
+	}
+
+	public int getRoundedPageWidth() {
+		int tmp = pageWidth / MERGE_VARIABILITY;
+		return tmp * MERGE_VARIABILITY;
+	}
+
+	public void choosePagesToMerge(List<Integer> pages) {
+		allPages = pages;
+		if (pages.size() < MAX_MERGE_PAGES) {
+			// use all pages
+			pagesToMerge = pages;
+		} else {
+			// use an equal distribution
+			float stepWidth = (float) pages.size() / MAX_MERGE_PAGES;
+			float totalStepped = 0;
+			for (int i = 0; i < MAX_MERGE_PAGES; i++) {
+				pagesToMerge.add(pages.get(new Double(Math.floor(totalStepped))
+						.intValue()));
+				totalStepped += stepWidth;
+			}
+		}
+	}
+
+	public List<Integer> getAllPages() {
+		return allPages;
+	}
+
+	public List<Integer> getPagesToMerge() {
+		return pagesToMerge;
+	}
+
+}
